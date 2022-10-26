bfce9acc2a288b9937d1e82a99d1f0246bf422c4
AgnesaS <agnesaselimi8@gmail.com>
2022-10-26 12:11:49 +0200

    Initial Commit

---
+++ SlotsChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsChallenge
+//
+//  Created by Agnesa  on 26.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlotsChallenge/SlotsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsChallengeApp.swift
+//  SlotsChallenge
+//
+//  Created by Agnesa  on 26.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
